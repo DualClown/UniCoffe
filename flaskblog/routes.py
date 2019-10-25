de8d2df80a4abbfe3beffd0fb4a66ed14802804a
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-10-25 11:28:34 -0500

    First commit

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -7,7 +7,7 @@ from flask_login import login_user, current_user, logout_user, login_required
 posts = [
     {
         'author': 'Manuel Lopez',
-        'title': 'Cafe Americano',
+        'title': 'Cafe Europeo',
         'content': '$200',
         'date_posted': 'September 20,2019'
     },
