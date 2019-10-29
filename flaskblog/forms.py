96748131576a6903cb1d230358e34237a662d61e
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-10-28 23:43:47 -0500

    first commit

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, length, email, EqualTo, ValidationError
 from flaskblog.models import User
 
