447954853f0ab4eb8990ff7b6b3435e4687b4e3b
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-11-01 19:25:54 -0500

    video 11 blueprints

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+
+class Config:
+    SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    EMAIL_USER = 'tiendadelcafe19@gmail.com'
+    EMAIL_PASS = 'admintdc'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, TextAreaField
-from wtforms.validators import DataRequired, length, email, EqualTo, ValidationError
-from flaskblog.models import User
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
-                           DataRequired(), length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), email()])
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[
                                      DataRequired(), EqualTo('password')])
@@ -29,7 +29,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
@@ -37,8 +37,8 @@ class LoginForm(FlaskForm):
 
 class UpdateAccountForm(FlaskForm):
     username = StringField('Username', validators=[
-                           DataRequired(), length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), email()])
+                           DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators=[
                         FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
@@ -71,14 +71,8 @@ class UpdateBalanceForm(FlaskForm):
                 "That username doesn't exist please choose a different one.")
 
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-
 class RequestResetForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), email()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
 
     def validate_email(self, email):
