5788cdf51c50509646710309737ed456d48840ca
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-11-01 19:49:48 -0500

    video 11 blueprints

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,8 +2,8 @@
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    EMAIL_USER = 'tiendadelcafe19@gmail.com'
-    EMAIL_PASS = 'admintdc'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USERNAME = 'tiendadelcafe19@gmail.com'
+    MAIL_PASSWORD = 'admintdc'
