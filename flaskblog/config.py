af12821b491a9aa8e1200b0928352a01496d8c38
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-11-03 01:15:19 -0500

    intento base de datos heroku

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,7 +1,7 @@
 
 class Config:
     SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
-    DATABASE_URL = 'postgres://rixpfyostssdju:a1e7842eb193cca8bc217454b093e07a0bf63650e05543cdbf06fce61a971e84@ec2-107-21-126-201.compute-1.amazonaws.com:5432/dce4h8cq2iqff'
+    SQLALCHEMY_DATABASE_URI = 'postgres://rixpfyostssdju:a1e7842eb193cca8bc217454b093e07a0bf63650e05543cdbf06fce61a971e84@ec2-107-21-126-201.compute-1.amazonaws.com:5432/dce4h8cq2iqff'
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
