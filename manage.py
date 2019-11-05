097185e23f5458da85dba0215c905f6d61e75118
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-11-05 12:53:13 -0500

    otro intento db

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from flaskblog import app
+from flaskblog import create_app
 
-
+app = create_app()
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
