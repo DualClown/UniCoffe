d9ddcecf772b4ca8207d054ef41b281943586ae9
DualClown <manuelalejandro.lopezbarrios@gmail.com>
2019-11-05 17:38:01 -0500

    casi final

--- flaskblog/posts/routes.py
+++ flaskblog/posts/routes.py
@@ -13,15 +13,18 @@ posts = Blueprint('posts', __name__)
 @login_required
 def new_post():
     form = PostForm()
-    if form.validate_on_submit():
-        post = Post(title=form.title.data,
-                    content=form.content.data, author=current_user)
-        db.session.add(post)
-        db.session.commit()
-        flash('Your post has been created!', 'success')
-        return redirect(url_for('main.home'))
-    return render_template('create_post.html', title='New Post',
-                           form=form, legend='New Post')
+    if current_user.username != "Admin":
+        abort(403)
+    else:
+        if form.validate_on_submit():
+            post = Post(title=form.title.data,
+                        content=form.content.data, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            flash('Your post has been created!', 'success')
+            return redirect(url_for('main.home'))
+        return render_template('create_post.html', title='New Post',
+                               form=form, legend='New Post')
 
 
 @posts.route("/post/<int:post_id>")
